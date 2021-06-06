2aa8e64ec9c9a404fe18d909c87bf66685b76b53
RuiOkazaki <okazaki200226@gmail.com>
2021-06-06 22:48:51 +0900

    Listから詳細を見れるようにした

---
+++ GitHubClient/Views/RepoDetailView.swift
@@ -0,0 +1,48 @@
+//
+//  RepoDetailView.swift
+//  GitHubClient
+//
+//  Created by 岡崎流依 on 2021/06/06.
+//
+
+import SwiftUI
+
+struct RepoDetailView: View {
+    let repo:Repo
+    
+    var body: some View {
+        ScrollView{
+            HStack{
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image("GitHubMark")
+                            .resizable()
+                            .frame(width: 16, height: 16)
+                        Text(repo.owner.name)
+                            .font(.caption)
+                    }
+                    Text(repo.name)
+                        .font(.body)
+                        .fontWeight(.bold)
+                    Text(repo.description)
+                        .padding(.top, 4)
+                    HStack {
+                        Image(systemName: "star")
+                        Text("\(repo.stargazersCount) stars")
+                    }
+                    .padding(.top, 8)
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding(16)
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct RepoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepoDetailView(repo: .mock1)
+    }
+}
