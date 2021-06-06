f3b9487665b327da2c14f36eb75a734b036d3745
RuiOkazaki <okazaki200226@gmail.com>
2021-06-06 21:11:18 +0900

    first commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by 岡崎流依 on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
